a9582604637a35b59c49ae3fb78af5da419541b6
Prudhvi <prudhvinaidu11111@gmail.com>
2021-06-09 23:47:14 +0200

    i have written the required code and test cases for my project. now i will make chages to the code and test cases

--- src/main/java/WeightConverter.java
+++ src/main/java/WeightConverter.java
@@ -5,14 +5,16 @@ public class WeightConverter {
     public int kg_to_grams(int kg){
         int gms = kg * 1000;
         //return gms;
+        return 0;
     }
     public int grams_to_mg(int gms){
         int mg = gms * 1000;
         //return mg;
+        return 0;
     }
     public int pds_to_kg(int pds){
         double kg = pds * 2;
         //return (int) kg;
-
+        return 0;
     }
 }
