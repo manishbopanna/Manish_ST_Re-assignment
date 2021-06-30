59baffa47ac10917c51b896bb1a7f636713180d1
Prudhvi <prudhvinaidu11111@gmail.com>
2021-07-01 00:38:40 +0200

    test case 3 with code.

--- src/main/java/WeightConverter.java
+++ src/main/java/WeightConverter.java
@@ -12,4 +12,9 @@ public class WeightConverter {
         int mg = gms *1000;
         return mg;
     }
+
+    public int pds_to_kg(int pds){
+        double kg =pds * 2;
+        return (int) kg;
+    }
 }
